7b7d3cbd9949c6e2441e0f3499fa385d8da5e170
nikolay-t16 <nikolay.ter@gmail.com>
2015-05-17 00:56:38 +0400

    кнопки с действиями для игроков

--- app/src/main/java/com/ter/nikolay/kaub/MainActivity.java
+++ app/src/main/java/com/ter/nikolay/kaub/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void checkPointCount(){
         int CheckPointCount = modelGame.checkPointCount();
         if(CheckPointCount>0){
-            stopGame("Команда "+Integer.toString(CheckPointCount)+" выйграла");
+            stopGame("Команда " + Integer.toString(CheckPointCount) + " выйграла");
         }
     }
     protected void checkFoulCount(){
@@ -138,6 +138,92 @@ public class MainActivity extends Activity implements View.OnClickListener {
             }
         }
     }
+    public void onClickPlayerAction(View v){
+        switch (v.getId()) {
+            case R.id.onePointPlayer1:
+                addPoint(1,1);
+                break;
+            case R.id.twoPointPlayer1:
+                addPoint(1,2);
+                break;
+            case R.id.foulPlayer1:
+                addFoul(1);
+                break;
+            case R.id.onePointPlayer2:
+                addPoint(2,1);
+                break;
+            case R.id.twoPointPlayer2:
+                addPoint(2, 2);
+                break;
+            case R.id.foulPlayer2:
+                addFoul(2);
+                break;
+            case R.id.onePointPlayer3:
+                addPoint(3,1);
+                break;
+            case R.id.twoPointPlayer3:
+                addPoint(3, 2);
+                break;
+            case R.id.foulPlayer3:
+                addFoul(3);
+                break;
+            case R.id.onePointPlayer4:
+                addPoint(4,1);
+                break;
+            case R.id.twoPointPlayer4:
+                addPoint(4, 2);
+                break;
+            case R.id.foulPlayer4:
+                addFoul(4);
+                break;
+            case R.id.onePointPlayer5:
+                addPoint(5,1);
+                break;
+            case R.id.twoPointPlayer5:
+                addPoint(5, 2);
+                break;
+            case R.id.foulPlayer5:
+                addFoul(5);
+                break;
+            case R.id.onePointPlayer6:
+                addPoint(6,1);
+                break;
+            case R.id.twoPointPlayer6:
+                addPoint(6,2);
+                break;
+            case R.id.foulPlayer6:
+                addFoul(6);
+                break;
+            case R.id.onePointPlayer7:
+                addPoint(7,1);
+                break;
+            case R.id.twoPointPlayer7:
+                addPoint(7,2);
+                break;
+            case R.id.foulPlayer7:
+                addFoul(7);
+                break;
+            case R.id.onePointPlayer8:
+                addPoint(4,1);
+                break;
+            case R.id.twoPointPlayer8:
+                addPoint(4,2);
+                break;
+            case R.id.foulPlayer8:
+                addFoul(4);
+                break;
+
+        }
+
+    }
+    protected void addPoint(int playerNum,int points){
+        TeamPointCount[playerNum < 5 ? 0 : 1].setText(Integer.toString(modelGame.addPoint(playerNum, points)));
+        checkPointCount();
+    }
+    protected void addFoul(int playerNum){
+        TeamFoulCount[playerNum < 5 ? 0 : 1].setText(Integer.toString(modelGame.addFoul(playerNum)));
+        checkFoulCount();
+    }
 
     protected void onClickPlayer(View v,int playerNum){
         playerSelected = playerNum;
@@ -151,17 +237,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
             public void onClick(DialogInterface dialog, int item) {
                 switch (item) {
                     case 0:
-                        TeamPointCount[playerSelected < 5 ? 0 : 1].setText(Integer.toString(modelGame.addPoint(playerSelected, 1)));
-                        checkPointCount();
+                        addPoint(playerSelected, 1);
                         break;
                     case 1:
-                        TeamPointCount[playerSelected < 5 ? 0 : 1].setText(Integer.toString(modelGame.addPoint(playerSelected, 2)));
-                        checkPointCount();
+                        addPoint(playerSelected, 2);
                         break;
                     case 2:
-                        TeamFoulCount[playerSelected < 5 ? 0 : 1].setText(Integer.toString(modelGame.addFoul(playerSelected)));
-
-                        break;
+                        addFoul(playerSelected);
+                         break;
 
                 }
             }
